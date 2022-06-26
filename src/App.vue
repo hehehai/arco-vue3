5944f9ddbfcfe86f31509b3733cb8dab7e23f966
hehehai <riverhohai@gmail.com>
2022-06-26 21:40:26 +0800

    fix: use head not found

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,21 @@
 <script setup lang="ts">
+// https://github.com/vueuse/head
+// you can use this to manipulate the document head in any components,
+// they will be rendered correctly in the html results with vite-ssg
+useHead({
+  title: 'Arco Vue3 template',
+  meta: [
+    { name: 'description', content: 'Arco Vue Starter Template' },
+  ],
+  link: [
+    {
+      rel: 'icon',
+      type: 'image/svg+xml',
+      href: '/favicon.svg',
+    },
+  ],
+})
+
 watchEffect(() => {
   if (isDark.value)
     document.documentElement.classList.add('dark')
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import { createHead } from '@vueuse/head'
 import App from './App.vue'
 
 import '@unocss/reset/tailwind.css'
@@ -5,6 +6,9 @@ import './styles/main.css'
 import 'uno.css'
 
 const app = createApp(App)
+
+const head = createHead() // <--
+app.use(head)
 
 Object.values(import.meta.globEager('./modules/*.ts')).forEach(i => i.install?.({ app }))
 
